7ff05c66081738b422fe00a2afd563cd063d8f09
Xavier Trinidad <xmtrinidad01@gmail.com>
2019-08-31 18:22:49 -0700

    Created front-end register forms

--- src/views/register/register-view.js
+++ src/views/register/register-view.js
@@ -3,7 +3,7 @@ import { LitElement, html, css } from 'lit-element';
 class RegisterView extends LitElement {
     static get properties() {
         return {
-            componentName: { type: String }
+            
         };
     }
 
@@ -24,7 +24,96 @@ class RegisterView extends LitElement {
 
     render() {
         return html`
-        <p>${this.componentName} works!</p>
+        <div id="inputContainer">
+            <form action="" id="loginForm" method="POST">
+                <h2>Login to your account</h2>
+                <div>
+                    <label for="loginUsername">Username</label>
+                    <input
+                        required 
+                        type="text"
+                        placeholder="e.g. bart simpson" 
+                        id="loginUsername" 
+                        name="loginUsername">
+                </div>
+                <div>
+                    <label for="loginPassword">Password</label>
+                    <input
+                        required 
+                        type="password"
+                        id="loginPassword" 
+                        name="loginPassword">
+                </div>
+                <button type="submit" name="loginButton">LOG IN</button>
+            </form>
+
+            <form action="" id="registerForm" method="POST">
+                <h2>Create your free account</h2>
+                <div>
+                    <label for="username">Username</label>
+                    <input
+                        required 
+                        type="text"
+                        placeholder="e.g. bart simpson" 
+                        id="username" 
+                        name="username">
+                </div>
+                <div>
+                    <label for="firstName">First Name</label>
+                    <input
+                        required 
+                        type="text"
+                        placeholder="e.g. Bart" 
+                        id="firstName" 
+                        name="firstName">
+                </div>
+                <div>
+                    <label for="lastName">Last Name</label>
+                    <input
+                        required 
+                        type="text"
+                        placeholder="e.g. Simpson" 
+                        id="lastName" 
+                        name="lastName">
+                </div>
+                <div>
+                    <label for="email">Email</label>
+                    <input
+                        required 
+                        type="email"
+                        placeholder="e.g. bart@gmail.com" 
+                        id="email" 
+                        name="email">
+                </div>
+                <div>
+                    <label for="email2">Confirm Email</label>
+                    <input
+                        required 
+                        type="email"
+                        placeholder="e.g. bart@gmail.com" 
+                        id="email2" 
+                        name="email2">
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input
+                        required 
+                        type="password"
+                        id="password" 
+                        name="password">
+                </div>
+                <div>
+                    <label for="password2">Confirm Password</label>
+                    <input
+                        required 
+                        type="password"
+                        id="password2" 
+                        name="password2">
+                </div>
+                <button type="submit" name="registerButton">Sign Up</button>
+            </form>
+
+        </div>
     `;
     }
 }
